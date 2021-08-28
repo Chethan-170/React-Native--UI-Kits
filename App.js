9be52fbbd8918cb5c6c9a52ecd7ed8f12b2fcfd2
Chethan N <chethan.n@niveussolutions.com>
2021-08-29 01:40:08 +0530

    popup final setup

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {StyleSheet, Text, View, Button, TextInput, StatusBar, TouchableWithoutFeedback, SafeAreaView} from 'react-native';
+import {StyleSheet, View, Button, TextInput, StatusBar, TouchableWithoutFeedback, SafeAreaView} from 'react-native';
 import MyBottomPopUp from './app/screens/MyBottomPopUp';
 
 export default function App() {
@@ -7,22 +7,48 @@ export default function App() {
   const onShowPopup = ()=>{
     popupRef.show();
   }
-  
+
   return(
     <View style={{ flex: 1}}>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-          <Button title="Click Me" onPress={onShowPopup} />
-          <MyBottomPopUp ref={(target) => popupRef = target}
-            title="My Popup"
-          >
-            <Text>Boom</Text>
-          </MyBottomPopUp>
+        <Button title="Click Me" onPress={onShowPopup} />
+        <MyBottomPopUp ref={(target) => popupRef = target}
+          title="LOGIN"
+        >
+          <View style={{padding: 4}}>
+            <View style={[styles.mt1]}>
+              <Button
+                onPress={()=>{}}
+                title="Login With Facebook"
+                color="#3B5998"
+              />
+            </View>
+            <View style={[styles.mt1]}>
+              <Button
+                onPress={()=>{}}
+                title="Login With Google"
+                color="grey"
+              />
+            </View>
+            <View style={[styles.mt1]}>
+              <Button
+                onPress={()=>{}}
+                title="Login with Mobile / Email"
+                color="#EE743C"
+              />
+            </View>
+          </View>
+        </MyBottomPopUp>
       </SafeAreaView>
     </View>
   );
 }
-
+const styles = StyleSheet.create({
+  mt1:{
+    marginTop: 15
+  }
+});
 
 //TODO:
 // Check Portal first
--- app/screens/MyBottomPopUp.js
+++ app/screens/MyBottomPopUp.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View, Modal, Dimensions, TouchableWithoutFeedback } from 'react-native'
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const deviceHeight = Dimensions.get('window').height;
 
@@ -27,7 +28,14 @@ export default class MyBottomPopUp extends React.Component {
     }
     renderTitle = ()=>{
         const { title } = this.props;
-        return <View><Text style={styles.titleText}> {title} </Text></View>
+        return (
+                <View style={styles.titleContainer}>
+                    <Text style={styles.titleText}> {title} </Text>
+                    <TouchableWithoutFeedback onPress={this.close}>
+                        <AntDesign name={'close'} size={25} />
+                    </TouchableWithoutFeedback>
+                </View>
+            );
     }
     renderContent = ()=>{
         const { children: content } = this.props;
@@ -43,12 +51,10 @@ export default class MyBottomPopUp extends React.Component {
                 onRequestClose={this.close}
             >
                 <View style={styles.container}>
-                    {this.renderOutsideTouchable()}
+                    {/* {this.renderOutsideTouchable()} */}
                     <View style={styles.bottomViewContainer}>
-                        <View>
-                            {this.renderTitle()}
-                            {this.renderContent()}
-                        </View>
+                        {this.renderTitle()}
+                        {this.renderContent()}
                     </View>
                 </View>
             </Modal>
@@ -61,18 +67,31 @@ const styles = StyleSheet.create({
         width: '100%',
         borderTopRightRadius: 10,
         borderTopLeftRadius: 10,
-        paddingHorizontal: 10,
+        paddingHorizontal: 15,
+        paddingVertical: 15,
         maxHeight: deviceHeight * 0.4
     },
     container:{
         flex: 1,
-        backgroundColor: '#89919B', 
+        backgroundColor: 'rgba(116, 117, 116, 0.5)',
         justifyContent:'flex-end',
+        alignItems: "center",
+        // shadowOffset: {
+        // width: 0,
+        // height: 2
+        // },
+        // shadowOpacity: 0.25,
+        // shadowRadius: 4,
+        // elevation: 5
+    },
+    titleContainer:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
     },
     titleText:{
         color: "#1B2E44",
         fontSize: 20,
-        fontWeight: '500',
-        margin: 15
+        fontWeight: '400'
     }
 });
